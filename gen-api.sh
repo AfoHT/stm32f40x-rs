7819379f7b293e6d45d63fa2e25c6cc3c0d4c142
Henrik Tjäder <henrik@tjaders.com>
2018-07-12 08:26:46 +0000

    generated with svd2rust 0.13, general version bump

--- gen-api.sh
+++ gen-api.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-svd2rust -i resources/STM32F40x.svd > src/lib.rs
+# Install form, svd2rust, rustfmt-nightly
+
+rm -rf src
+
+svd2rust --nightly -i resources/STM32F40x.svd
 
 # Reformat compiler attributes removing unnecessary spaces
 # Remove spaces from # [ attribute ] => #[attribute] and add \n
-sed -i 's/\s*# \[ \([^]]*\) \]/\n#[\1]/g' src/lib.rs
+sed -i 's/\s*# \[ \([^]]*\) \]/\n#[\1]/g' lib.rs
 # Remove spaces from # ! [ attribute ] and add \n
-sed -i 's/\s*# ! \[ \([^]]*\) \]/#![\1]\n/g' src/lib.rs
-sed -i 's/ \([()]\) */\1/g' src/lib.rs
+sed -i 's/\s*# ! \[ \([^]]*\) \]/#![\1]\n/g' lib.rs
+sed -i 's/ \([()]\) */\1/g' lib.rs
+
+form -i lib.rs -o src/ && rm lib.rs
 
 # Use rustfmt to reformat to human readable format
-rustfmt src/*.rs
+#rustfmt src/*.rs
+cargo fmt
 
 # Test that build succeeds for target platform (ARM Cortex-M4)
 xargo check --target thumbv7em-none-eabihf
